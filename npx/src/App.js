d0f7270f1508aa8eca0e129d06412749b80a341c
yijie010301 <70183979+yijie010301@users.noreply.github.com>
2023-11-17 11:53:01 -0600

    first page

--- npx/src/App.js
+++ npx/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to my page</h1>
       </header>
+      <section>
+        <h2>About Me</h2>
+        <p>Personal Infomation</p>
+      </section>
+      <section>
+        <h2>Contact</h2>
+        <p>Email: yijie4@illinois.edu</p>
+      </section>
     </div>
   );
 }
